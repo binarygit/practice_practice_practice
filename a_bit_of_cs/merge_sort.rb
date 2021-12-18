1a33e0d5af28718fb188c1d9620b2ea2b72899bf
binarygit <inscription4@outlook.com>
2021-12-18 13:19:58 +0545

    refactor

--- a_bit_of_cs/merge_sort.rb
+++ a_bit_of_cs/merge_sort.rb
@@ -1,29 +1,27 @@
 #!/usr/bin/ruby
-require 'pry-byebug'
 
 class MergeSort
   def sort(array)
     return array if array.size.eql?(1) || array.empty?
+
     sorted_array = []
-    divisor = array.size/2
+    length = start = array.size / 2
 
-    left_array = array[0, divisor]
-    right_array = array[divisor..-1]
+    left_array = sort(array[0, length])
+    right_array = sort(array[start..-1])
 
-    left_array = sort(left_array)
-    right_array = sort(right_array)
-
-    i = 0
-    j = 0
     loop do
-      (sorted_array.push(right_array[i..-1]); break) if left_array.size.eql?(j)
-      (sorted_array.push(left_array[j..-1]); break) if right_array.size.eql?(i)
-      (sorted_array.push(left_array[j]); j += 1; next) if left_array[j] < right_array[i]
-      sorted_array.push(right_array[i])
-      i += 1
+      break if left_array.empty? && right_array.empty?
+
+      (sorted_array << right_array.shift; next) if left_array.empty?
+      (sorted_array << left_array.shift; next) if right_array.empty?
+
+      case left_array <=> right_array
+      when -1 then sorted_array << left_array.shift
+      when 1 then sorted_array << right_array.shift
+      else        sorted_array << left_array.shift end
     end
 
-    sorted_array.flatten
+    sorted_array
   end
 end
-
